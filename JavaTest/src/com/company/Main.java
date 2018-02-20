30cb6625fa26ea7826cb963153737e7a31988c2d
Joseph Cui <joeychui@icloud.com>
2018-02-20 19:52:24 +0800

    usual

--- JavaTest/src/com/company/Main.java
+++ JavaTest/src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main{
     public static void main(String[] args){
 	// write your code here
         System.out.println(434);
+        System.out.println("ewe");
+        System.out.println("dsfghfdsf");
+
     }
 }
