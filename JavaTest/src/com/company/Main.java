be3f86da94a24ce88b33ff6a7dbc06a59e27d47e
Joseph Cui <joeychui@icloud.com>
2018-03-11 12:27:44 +0800

    usual

--- JavaTest/src/com/company/Main.java
+++ JavaTest/src/com/company/Main.java
@@ -3,10 +3,9 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         char[] i = {'d', 'r', 't', 'y', 'u'};
         System.out.println(i);
-         int j;
         System.out.println(j);
 
     }
